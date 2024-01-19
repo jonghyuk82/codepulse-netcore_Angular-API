027dded77552b3b4848d3ff0bd5927d4146cb9ab
John Lee <john.lee@wfs.aero>
2024-01-19 15:29:32 -0500

    Add project files.

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using CodePulse.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+
+namespace CodePulse.API.Controllers
+{
+    // https://localhost:xxxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        // 
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            // Domain Model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
